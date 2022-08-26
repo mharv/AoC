e82024ac46385e107113167a681d3ebd832c66f2
mharv <mitch.k.harvey@gmail.com>
2022-08-26 10:03:56 +1000

    day 3 complete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -33,4 +33,65 @@ fn main() {
     let epsilon_rate = i32::from_str_radix(&epsilon_rate_bin_str, 2).expect("Not a binary number!");
 
     println!("power consumption is: {}", gamma_rate*epsilon_rate);
+
+    // part 2
+    // get most frequent at bit one
+    let mut diag_report_for_oxy = content_split.clone();
+    let mut bit_of_interest: usize = 0;
+
+    while diag_report_for_oxy.len() > 1 {
+        let mut one_occuring = 0;
+        let mut zero_occuring = 0;
+        for binary_str in diag_report_for_oxy.iter() {
+            if binary_str.len() == 12 {
+                if binary_str.chars().nth(bit_of_interest).unwrap() == '1' {
+                    one_occuring += 1;
+                } else {
+                    zero_occuring += 1;
+                }
+            }
+        }
+        if one_occuring >= zero_occuring {
+            diag_report_for_oxy = diag_report_for_oxy.into_iter().filter(|x| x.chars().nth(bit_of_interest).unwrap() == '1').collect();
+        } else {
+            diag_report_for_oxy = diag_report_for_oxy.into_iter().filter(|x| x.chars().nth(bit_of_interest).unwrap() == '0').collect();
+        }
+        bit_of_interest += 1;
+    }
+
+    println!("oxygen generator rating report length is: {}", diag_report_for_oxy.len());
+    println!("oxygen generator rating is: {}", diag_report_for_oxy[0]);
+
+    // get least frequent at bit one
+    let mut diag_report_for_co2 = content_split.clone();
+    let mut bit_of_interest: usize = 0;
+
+    while diag_report_for_co2.len() > 1 {
+        let mut one_occuring = 0;
+        let mut zero_occuring = 0;
+        for binary_str in diag_report_for_co2.iter() {
+            if binary_str.len() == 12 {
+                if binary_str.chars().nth(bit_of_interest).unwrap() == '1' {
+                    one_occuring += 1;
+                } else {
+                    zero_occuring += 1;
+                }
+            }
+        }
+        if one_occuring < zero_occuring {
+            diag_report_for_co2 = diag_report_for_co2.into_iter().filter(|x| x.chars().nth(bit_of_interest).unwrap() == '1').collect();
+        } else {
+            diag_report_for_co2 = diag_report_for_co2.into_iter().filter(|x| x.chars().nth(bit_of_interest).unwrap() == '0').collect();
+        }
+        bit_of_interest += 1;
+    }
+
+    println!("co2 scrubber rating report length is: {}", diag_report_for_co2.len());
+    println!("co2 scrubber rating is: {}", diag_report_for_co2[0]);
+
+    // get results
+    let oxygen_generator_rating = i32::from_str_radix(&diag_report_for_oxy[0], 2).expect("Not a binary number!");
+    let co2_scrubber_rating = i32::from_str_radix(&diag_report_for_co2[0], 2).expect("Not a binary number!");
+
+    println!("life support rating is: {}", oxygen_generator_rating*co2_scrubber_rating);
 }
