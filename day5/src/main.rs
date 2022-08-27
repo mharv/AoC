4c6c7ad78be17cd17c6db4b516b64400c3c03691
mharv <mitch.k.harvey@gmail.com>
2022-08-28 09:29:24 +1000

    day5 part one complete

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,8 +2,8 @@ use std::fs;
 
 #[derive(Debug)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: usize,
+    y: usize,
 }
 
 #[derive(Debug)]
@@ -22,6 +22,31 @@ impl Grid {
     fn new() -> Self {
         let matrix = vec![vec![0; GRID_SIZE]; GRID_SIZE];
         Grid { matrix }
+    }
+
+    fn mark_line(&mut self, line: &Line) {
+        // only horizontal or vertical lines
+        if line.start_point.x == line.end_point.x || line.start_point.y == line.end_point.y {
+            for y in 0..self.matrix.len() {
+                for x in 0..self.matrix[y].len() {
+                    if y >= line.start_point.y && y <= line.end_point.y && x >= line.start_point.x && x <= line.end_point.x {
+                        self.matrix[y][x] += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    fn check_for_overlaps(&self) -> i32 {
+        let mut count = 0;
+        for y in 0..self.matrix.len() {
+            for x in 0..self.matrix[y].len() {
+                if self.matrix[y][x] >= 2 {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 
@@ -44,18 +69,27 @@ fn main() {
         let ep_x = temp_end_point[0].parse().expect("not a number");
         let ep_y = temp_end_point[1].parse().expect("not a number");
 
-        let start_point = Point { x: sp_x, y: sp_y };
-        let end_point = Point { x: ep_x , y: ep_y };
+        let start_point: Point;
+        let end_point: Point;
+
+        // will this make the start point values always be lower
+        // than the endpoint ones?
+        if sp_y > ep_y || sp_x > ep_x {
+            start_point = Point { x: ep_x, y: ep_y };
+            end_point = Point { x: sp_x , y: sp_y };
+        } else {
+            start_point = Point { x: sp_x, y: sp_y };
+            end_point = Point { x: ep_x , y: ep_y };
+        }
 
         let temp_line = Line { id: i as i32, start_point, end_point };
         lines.push(temp_line);
     }
 
-
+    let mut grid = Grid::new();
     for line in lines.iter() {
-        println!("{:?}", line);
+        grid.mark_line(&line);
     }
-
-    let grid = Grid::new();
-    println!("{:?}", grid);
+    let result = grid.check_for_overlaps();
+    println!("there are {} overlaps", result);
 }
