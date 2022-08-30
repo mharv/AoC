4cd6099d3a4327d2be805f1f780ea79f7f7f0178
mharv <mitch.k.harvey@gmail.com>
2022-08-30 19:01:02 +1000

    finished day 6, cheers GC (not garbage collector)

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,27 +11,37 @@ fn main() {
     }
 
     println!("there are now {} lanternfish", fish_list.len());
-    fish_list = simulate_n_days(fish_list, 30);
-    println!("there are now {} lanternfish", fish_list.len());
+    let number_of_fish = simulate_n_days(&mut fish_list, 256);
+    println!("there are now {} lanternfish", number_of_fish );
 
 }
 
-fn simulate_n_days(mut fish_list: Vec<u8>, n: i32) -> Vec<u8> {
-    for i in 0..n {
-        let mut new_fish: Vec<u8> = Vec::new();
-        let mut last_day_population = fish_list.len();
-        for fish in fish_list.iter_mut() {
-            if *fish == 0 {
-                *fish = 6;
-                new_fish.push(8);
-            } else {
-                *fish -= 1;
-            }
+fn simulate_n_days(fish_list: &mut Vec<u8>, n: i32) -> i64 {
+    let mut lifetime_tracker: Vec<i64> = vec![0; 9];
+
+    for fish in fish_list.iter() {
+        match fish {
+            0 => lifetime_tracker[0] += 1,
+            1 => lifetime_tracker[1] += 1,
+            2 => lifetime_tracker[2] += 1,
+            3 => lifetime_tracker[3] += 1,
+            4 => lifetime_tracker[4] += 1,
+            5 => lifetime_tracker[5] += 1,
+            6 => lifetime_tracker[6] += 1,
+            7 => lifetime_tracker[7] += 1,
+            8 => lifetime_tracker[8] += 1,
+            _ => panic!("derp number in input found!"),
         }
-        fish_list.append(&mut new_fish);
-
-        println!("day {} number of fish added: {}, number of fish total: {}", i, fish_list.len()-last_day_population, fish_list.len());
-
     }
-    fish_list
+
+    let mut last_lifetime_tracker = lifetime_tracker.clone();
+    for _ in 0..n {
+        for i in (1..last_lifetime_tracker.len()).rev() {
+            lifetime_tracker[i-1] = last_lifetime_tracker[i];
+        }
+        lifetime_tracker[6] += last_lifetime_tracker[0];
+        lifetime_tracker[8] = last_lifetime_tracker[0];
+        last_lifetime_tracker = lifetime_tracker.clone();
+    }
+    last_lifetime_tracker.iter().sum()
 }
