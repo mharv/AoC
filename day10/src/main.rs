2b926494f6e9c40d18f06b2a61477431808431f0
mharv <mitch.k.harvey@gmail.com>
2022-09-02 15:50:24 +1000

    finished day 10, part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -13,10 +13,32 @@ fn main() {
 
     let mut stack: Vec<Character> = Vec::new();
     let mut illegal_chars: Vec<char> = Vec::new();
+    let mut incomplete_line_indexes: Vec<usize> = Vec::new();
 
-    for line in content_split.iter() {
-        for character in line.chars() {
+    for (index, line) in content_split.iter().enumerate() {
+        for (i, character) in line.chars().enumerate() {
+            match character {
+                '[' => stack.push(Character { character, closing_character: ']' }),
+                '<' => stack.push(Character { character, closing_character: '>' }),
+                '{' => stack.push(Character { character, closing_character: '}' }),
+                '(' => stack.push(Character { character, closing_character: ')' }),
+                ']' => if character_check(&mut stack, &mut illegal_chars, character) == 0 { break; },
+                '>' => if character_check(&mut stack, &mut illegal_chars, character) == 0 { break; },
+                '}' => if character_check(&mut stack, &mut illegal_chars, character) == 0 { break; },
+                ')' => if character_check(&mut stack, &mut illegal_chars, character) == 0 { break; },
+                _ => (),
+            }
+            if i == line.len() - 1 {
+                incomplete_line_indexes.push(index);
+            }
+        }
+    }
 
+    let mut scores: Vec<i64> = Vec::new();
+
+    for i in incomplete_line_indexes.iter() {
+        let mut stack: Vec<Character> = Vec::new();
+        for character in content_split[*i].chars() {
             match character {
                 '[' => stack.push(Character { character, closing_character: ']' }),
                 '<' => stack.push(Character { character, closing_character: '>' }),
@@ -29,7 +51,24 @@ fn main() {
                 _ => (),
             }
         }
+
+        let mut local_score: i64 = 0;
+        for character in stack.iter().rev() {
+            local_score *= 5;
+            match character.closing_character {
+                ']' => local_score += 2,
+                '>' => local_score += 4,
+                '}' => local_score += 3,
+                ')' => local_score += 1,
+                _ => (),
+            };
+        }
+        scores.push(local_score);
+
+        stack.clear();
     }
+
+    scores.sort();
 
     let mut total_score = 0;
 
@@ -43,7 +82,8 @@ fn main() {
         }
     }
 
-    println!("final score: {}", total_score);
+    println!("final score part 1: {}", total_score);
+    println!("final score part 2: {:?}", scores[scores.len()/2]);
 }
 
 fn character_check(stack: &mut Vec<Character>, illegals: &mut Vec<char>, character: char) -> i32 {
