46cd8e7efe70d3e174efcbefbb1c3be74dda5b0d
mharv <mitch.k.harvey@gmail.com>
2022-09-03 09:02:33 +1000

    setup day11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // let path = String::from("./input.txt");
+    let path = String::from("./test_input.txt");
+    let content = fs::read_to_string(path).expect("file was not read");
+    let content_split: Vec<&str> = content.trim().split("\n").collect();
+    println!("{:?}", content_split);
 }
---
+++ day11/target/debug/day11.d
@@ -0,0 +1,1 @@
+/home/mharv/projects/aoc/day11/target/debug/day11: /home/mharv/projects/aoc/day11/src/main.rs
