a5cdd1f2b3ff754fb794f462df45239f9275478a
mharv <mitch.k.harvey@gmail.com>
2022-09-06 13:41:13 +1000

    current attempt is weird, strange outcomes when choosing nodes to go to. may need to start a fresh

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -7,6 +7,41 @@ struct Cave {
     connections: Vec<String>,
 }
 
+struct Agent {
+    current_location: String,
+    path_history: Vec<Vec<String>>,
+}
+
+impl Agent {
+    fn new() -> Self {
+        Agent { current_location: "start".to_string(), path_history: Vec::new() }
+    }
+
+    fn walk_cave(&mut self, cave_network: &HashMap<String, Cave>) {
+        let mut new_trip: Vec<String> = Vec::new();
+        new_trip.push(cave_network.get(&self.current_location).unwrap().name.to_string());
+
+        while self.current_location != "end" {
+            // get current_location options
+            // choose an option to navigate to
+            // check if option isnt big and hasnt been visited before
+            for option in cave_network.get(&self.current_location).unwrap().connections.iter() {
+                println!("possible options for {}: {:?}", self.current_location, cave_network.get(&self.current_location).unwrap().connections);
+                //println!("option for {}: {}", cave_network.get(&self.current_location).unwrap().name, option);
+                if !cave_network.get(option).unwrap().is_big && !new_trip.contains(&option) {
+                    self.current_location = cave_network.get(option).unwrap().name.to_string();
+                    println!("pushing option {} to trip", cave_network.get(option).unwrap().name.to_string());
+                    new_trip.push(cave_network.get(option).unwrap().name.to_string());
+                }
+            }
+
+            // update new position and push position to new_trip
+        }
+        self.path_history.push(new_trip);
+        println!("{:?}", self.path_history);
+    }
+}
+
 fn main() {
 
     let path = String::from("./test_input.txt");
@@ -14,6 +49,34 @@ fn main() {
     let content = fs::read_to_string(path).expect("file was not read");
     let content_split: Vec<&str> = content.trim().split("\n").collect();
 
+    let caves = construct_cave_network(content_split);
+
+    let mut agent = Agent::new();
+    agent.walk_cave(&caves);
+
+    // display cave
+    if true {
+        for (k, v) in caves.iter() {
+            if k == "d" {
+                print!("Cave name: {}\t", v.name);
+                println!("Cave is big: {}", v.is_big);
+                println!("Cave connections:");
+                for connection in v.connections.iter() {
+                    println!("\t{}", connection);
+                }
+                print!("\n");
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
+fn construct_cave_network(content_split: Vec<&str> ) -> HashMap<String, Cave> {
     let mut caves: HashMap<String, Cave> = HashMap::new();
 
     for row in content_split.iter() {
@@ -26,9 +89,7 @@ fn main() {
         } else {
             let mut is_big = false;
 
-            if temp_caves[0].to_uppercase() == temp_caves[0] {
-                is_big = true;
-            }
+            if temp_caves[0].to_uppercase() == temp_caves[0] { is_big = true; }
             let mut connections: Vec<String> = Vec::new();
             connections.push(temp_caves[1].to_string());
 
@@ -42,23 +103,12 @@ fn main() {
         } else {
             let mut is_big = false;
 
-            if temp_caves[1].to_uppercase() == temp_caves[1] {
-                is_big = true;
-            }
+            if temp_caves[1].to_uppercase() == temp_caves[1] { is_big = true; }
             let mut connections: Vec<String> = Vec::new();
             connections.push(temp_caves[0].to_string());
 
             caves.insert(temp_caves[1].to_string(), Cave { name: temp_caves[1].to_string(), is_big, connections });
         }
     }
-
-    for (_k, v) in caves.iter() {
-        print!("Cave name: {}\t", v.name);
-        println!("Cave is big: {}", v.is_big);
-        println!("Cave connections:");
-        for connection in v.connections.iter() {
-            println!("\t{}", connection);
-        }
-        print!("\n");
-    }
+    caves
 }
