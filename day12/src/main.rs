668d70c7fd0996abed2938ef20a2ab8098727257
mharv <mitch.k.harvey@gmail.com>
2022-09-06 11:08:36 +1000

    created cave system and input data processing for day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,68 @@
+use std::{fs, collections::HashMap};
+
+#[derive(Debug)]
+struct Cave {
+    name: String,
+    is_big: bool,
+    connections: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let path = String::from("./test_input.txt");
+    // let path = String::from("./input.txt");
+    let content = fs::read_to_string(path).expect("file was not read");
+    let content_split: Vec<&str> = content.trim().split("\n").collect();
+
+    let mut caves: HashMap<String, Cave> = HashMap::new();
+
+    for row in content_split.iter() {
+        let temp_caves: Vec<&str> = row.split("-").collect();
+
+        if caves.contains_key(temp_caves[0]) {
+            println!("{} already exists.", temp_caves[0]);
+            if !caves.get(temp_caves[0]).unwrap().connections.contains(&temp_caves[1].to_string()) {
+                caves.get_mut(temp_caves[0]).unwrap().connections.push(temp_caves[1].to_string());
+            }
+        } else {
+            println!("{} is not in the list of caves.", temp_caves[0]);
+            let mut is_big = false;
+
+            if temp_caves[0].to_uppercase() == temp_caves[0] {
+                is_big = true;
+            }
+            let mut connections: Vec<String> = Vec::new();
+            connections.push(temp_caves[1].to_string());
+
+            caves.insert(temp_caves[0].to_string(), Cave { name: temp_caves[0].to_string(), is_big, connections });
+        }
+
+        if caves.contains_key(temp_caves[1]) {
+            println!("{} already exists.", temp_caves[1]);
+            if !caves.get(temp_caves[1]).unwrap().connections.contains(&temp_caves[0].to_string()) {
+                caves.get_mut(temp_caves[1]).unwrap().connections.push(temp_caves[0].to_string());
+            }
+        } else {
+            println!("{} is not in the list of caves.", temp_caves[1]);
+            let mut is_big = false;
+
+            if temp_caves[1].to_uppercase() == temp_caves[1] {
+                is_big = true;
+            }
+            let mut connections: Vec<String> = Vec::new();
+            connections.push(temp_caves[0].to_string());
+
+            caves.insert(temp_caves[1].to_string(), Cave { name: temp_caves[1].to_string(), is_big, connections });
+        }
+    }
+
+    for (k, v) in caves.iter() {
+        println!("Cave name: {}", v.name);
+        println!("Cave is big: {}", v.is_big);
+        println!("Cave connections:");
+        for connection in v.connections.iter() {
+            println!("\t{}", connection);
+        }
+        print!("\n");
+    }
 }
---
+++ day12/target/debug/day12.d
@@ -0,0 +1,1 @@
+/home/mharv/projects/aoc/day12/target/debug/day12: /home/mharv/projects/aoc/day12/src/main.rs
