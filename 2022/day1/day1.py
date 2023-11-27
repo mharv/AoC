293f161643e00456a1bb1eddf3a01182083c427b
mharv <mitch.k.harvey@gmail.com>
2023-11-27 11:39:16 +0000

    added 2022 scaffolding

---
+++ 2022/day1/day1.py
@@ -0,0 +1,11 @@
+# Description: Advent of Code: Day 1, 2022
+# Created by: Mitchell Harvey
+
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/template.py
@@ -0,0 +1,9 @@
+# Description: Advent of Code: Day X, 2022
+# Created by: Mitchell Harvey
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
