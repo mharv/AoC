00d270c21e3fac52521f74e0c813423da90d98b8
mharv <mitch.k.harvey@gmail.com>
2023-11-27 22:16:08 +0000

    update thet template

--- 2022/template.py
+++ 2022/template.py
@@ -1,6 +1,12 @@
 # Description: Advent of Code: Day X, 2022
 # Created by: Mitchell Harvey
 
+
+def read_input():
+    with open("./input.txt") as file:
+        return file.read()
+
+
 def main():
     print("Hello World!")
 
