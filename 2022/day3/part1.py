1b13b16dda14a09a3d853ea2b367062b5ca3c9aa
mharv <mitch.k.harvey@gmail.com>
2023-11-30 20:34:30 +0000

    we finished day3 part 1 2022, we rule!

---
+++ 2022/day3/part1.py
@@ -0,0 +1,97 @@
+# Description: Advent of Code: Day X, 2022
+# Created by: Mitchell Harvey
+
+example = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+lookup = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+}
+
+
+def read_input():
+    with open("./input.txt") as file:
+        return file.read()
+
+
+def main():
+    results = []
+    for line in read_input().splitlines():
+        length = len(line)
+        half_length = int(length / 2)
+
+        char = ""
+
+        for fchar in line[:half_length]:
+            for schar in line[half_length:length]:
+                if fchar == schar:
+                    char = schar
+                    break
+            if char != "":
+                break
+        results.append(char)
+
+        total = 0
+        for result in results:
+            total += lookup[result]
+
+        print(total)
+
+
+if __name__ == "__main__":
+    main()
