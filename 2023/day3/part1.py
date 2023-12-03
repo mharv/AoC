fea57be1f98f6712c8d91aa84f39e641909ac803
mharv <mitch.k.harvey@gmail.com>
2023-12-03 08:05:56 +0000

    day3 2023 prep

---
+++ 2023/day3/part1.py
@@ -0,0 +1,20 @@
+# Description: Advent of Code: Day 3, 2023
+# Created by: Mitchell Harvey
+
+example = """"""
+
+
+def read_input():
+    # return example
+    with open("./input.txt") as file:
+        return file.read()
+
+
+def main():
+    total = 0
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
