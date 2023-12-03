0c0417b4e33b2bfee591452c6a4cc6a2f6a5fa5a
mharv <mitch.k.harvey@gmail.com>
2023-12-03 22:04:12 +0000

    ready d4

---
+++ 2023/day4/part2.py
@@ -0,0 +1,18 @@
+# Description: Advent of Code: Day 4, 2023
+# Created by: Mitchell Harvey
+
+example = """"""
+
+
+def read_input():
+    # return example
+    with open("./input.txt") as file:
+        return file.read()
+
+
+def main():
+    input = read_input()
+
+
+if __name__ == "__main__":
+    main()
