0252ffa8cbaf8fe20d13d16645578e70c7ff8659
mharv <mitch.k.harvey@gmail.com>
2023-12-05 08:23:50 +0000

    day5 part 2 fail!

--- 2023/day5/part2.py
+++ 2023/day5/part2.py
@@ -36,21 +36,67 @@ humidity-to-location map:
 56 93 4"""
 
 
-class Game:
-    def __init__(self, game_no, winning_numbers=[], my_numbers=[]):
-        self.game_no = game_no
-        self.winning_numbers = []
-        self.my_numbers = []
+class ConversionMap:
+    def __init__(self, name):
+        self.name = name
+        self.maps = []
+
+    def process(self, value):
+        # 0: destination, 1: source, 2: range
+        for map in self.maps:
+            if value in range(map[1], map[1] + map[2]):
+                return value + (map[0] - map[1])
+        return value
+
+
+def create_maps_seeds(input):
+    seeds = []
+    maps = []
+    for i, line in enumerate(input.splitlines()):
+        temp_map = ConversionMap("temp")
+        if line == "":
+            continue
+        if i == 0:
+            seed_string = line.split(": ")[1].split()
+            for seed_index in range(0, len(seed_string), 2):
+                # print(seed_string[seed_index])
+                # print(seed_string[seed_index + 1])
+                for seed_no in range(
+                    int(seed_string[seed_index]),
+                    int(seed_string[seed_index]) + int(seed_string[seed_index + 1]),
+                ):
+                    seeds.append(int(seed_no))
+
+        else:
+            if ":" in line:
+                temp_map.name = line.split()[0]
+                maps.append(temp_map)
+            else:
+                # map construction logic
+                maps[len(maps) - 1].maps.append(
+                    (int(line.split()[0]), int(line.split()[1]), int(line.split()[2]))
+                )
+
+    return seeds, maps
 
 
 def read_input():
-    # return example
+    return example
     with open("./input.txt") as file:
         return file.read()
 
 
 def main():
-    return 0
+    seeds, maps = create_maps_seeds(read_input())
+    # print(seeds)
+    targets = []
+    for seed in seeds:
+        target = seed
+        for map in maps:
+            target = map.process(target)
+        targets.append(target)
+    # print(targets)
+    print(min(targets))
 
 
 if __name__ == "__main__":
