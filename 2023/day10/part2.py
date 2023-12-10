50301eae5fb3d8b88c225f152ea8c790eb0d8385
mharv <mitch.k.harvey@gmail.com>
2023-12-10 12:22:21 +0000

    p2 done

--- 2023/day10/part2.py
+++ 2023/day10/part2.py
@@ -1,16 +1,26 @@
 # Description: Advent of Code: Day 10, 2023
 # Created by: Mitchell Harvey
 
-example = """FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L"""
+# example = """FF7FSF7F7F7F7F7F---7
+# L|LJ||||||||||||F--J
+# FL-7LJLJ||||||LJL-77
+# F--JF--7||LJLJ7F7FJ-
+# L---JF-JLJ.||-FJLJJ7
+# |F|F-JF---7F7-L7L|7|
+# |FFJF7L7F-JF7|JL---7
+# 7-L-JL7||F7|L7F-7F7|
+# L.L7LFJ|||||FJL7||LJ
+# L7JLJL-JLJLJL--JLJ.L"""
+
+example = """...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."""
 
 
 def process_input(input):
@@ -31,7 +41,8 @@ def find_start(matrix):
 
 
 def find_next_pipe(matrix, position, previous_position):
-    # choose start tile ( this was over engineered)
+    # choose start tile ( this was over engineered) direction that is
+    # chosen dependant on order of execution below
     if matrix[position[0]][position[1]] == "S":
         if not position[1] - 1 < 0:
             if matrix[position[0]][position[1] - 1] in "L-FS" and (
@@ -63,7 +74,6 @@ def find_next_pipe(matrix, position, previous_position):
 
     # once you are moving, determine exit based on pipe shape
     else:
-        print(matrix[position[0]][position[1]])
         if matrix[position[0]][position[1]] == "J":
             if previous_position[1] != position[1] - 1:
                 return (position[0], position[1] - 1), position  # Left
@@ -110,22 +120,45 @@ def read_input():
 def main():
     matrix = process_input(read_input())
     start = find_start(matrix)
-    home = start
 
-    print(start)
-    count = 0
+    # initialize a random cell on the matrix, probably should not be
+    # on matrix in case it happens to be near S by some small chance
     previous_position = (len(matrix) - 1, len(matrix[0]) - 1)
     current_position = start
 
+    tunnel = []
     while True:
         current_position, previous_position = find_next_pipe(
             matrix, current_position, previous_position
         )
+        tunnel.append(current_position)
 
         if current_position == start:
             break
-        count += 1
-    print((count + 1) / 2)
+
+    # create a simple overlay matrix "marker" that can be used to denote path (as a cell == 1)
+    marker = []
+    for i in matrix:
+        marker.append([0] * len(i))
+    for item in tunnel:
+        marker[item[0]][item[1]] = 1
+
+    contained = []
+    for i, row in enumerate(marker):
+        toggle = False
+        for j, char in enumerate(row):
+            # if i hit the path, make sure i am crossing it and not following it.
+            if (
+                char == 1 and matrix[i][j] in "JL|S"
+            ):  # S must be included because it is considered vertical in my input
+                toggle = not toggle
+                # crossed path
+
+            if toggle and char == 0:
+                # any empty cell that is found once the path is crossed must be internal
+                contained.append((i, j))
+
+    print(len(contained))
 
 
 if __name__ == "__main__":
