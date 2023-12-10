399ba67c7b4614be715ae09e71fc1bc2f7e6df6c
mharv <mitch.k.harvey@gmail.com>
2023-12-10 12:24:16 +0000

    placeholders

---
+++ 2023/day11/part1.py
@@ -0,0 +1,27 @@
+# Description: Advent of Code: Day 11, 2023
+# Created by: Mitchell Harvey
+
+example = """"""
+
+
+def process_input(input):
+    return input
+
+
+def apply_instructions(instructions):
+    print(instructions)
+
+
+def read_input():
+    # return example
+    with open("./input.txt") as file:
+        return file.read()
+
+
+def main():
+    # apply_instructions(instructions, mapp, 0, start_position)
+    print("notuso")
+
+
+if __name__ == "__main__":
+    main()
