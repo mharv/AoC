11465998887c52280eaca46a463c44ff086b73cc
mharv <mitch.k.harvey@gmail.com>
2023-12-11 07:03:29 +0000

    d11p1 find glaxaies to expand

--- 2023/day11/part1.py
+++ 2023/day11/part1.py
@@ -1,26 +1,49 @@
 # Description: Advent of Code: Day 11, 2023
 # Created by: Mitchell Harvey
 
-example = """"""
+example = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
 
 
 def process_input(input):
     return input
 
 
-def apply_instructions(instructions):
-    print(instructions)
+def expand(input):
+    rows_to_expand = []
+    cols_to_expand = []
+    for i, line in enumerate(input.splitlines()):
+        if all(char == "." for char in line):
+            rows_to_expand.append(i)
+    # loop through columns
+    for i in range(len(input.splitlines()[0])):
+        column = ""
+        for line in input.splitlines():
+            column += line[i]
+        if all(char == "." for char in column):
+            cols_to_expand.append(i)
+    return rows_to_expand, cols_to_expand
 
 
 def read_input():
-    # return example
+    return example
     with open("./input.txt") as file:
         return file.read()
 
 
 def main():
-    # apply_instructions(instructions, mapp, 0, start_position)
-    print("notuso")
+    input = read_input()
+    for line in input.splitlines():
+        print(line)
+    print(expand(input))
 
 
 if __name__ == "__main__":
