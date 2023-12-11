551edc68f40cac8d4cfe24c833215e18d204255f
mharv <mitch.k.harvey@gmail.com>
2023-12-11 07:15:40 +0000

    expansion complete

--- 2023/day11/part1.py
+++ 2023/day11/part1.py
@@ -23,14 +23,29 @@ def expand(input):
     for i, line in enumerate(input.splitlines()):
         if all(char == "." for char in line):
             rows_to_expand.append(i)
-    # loop through columns
+
     for i in range(len(input.splitlines()[0])):
         column = ""
         for line in input.splitlines():
             column += line[i]
         if all(char == "." for char in column):
             cols_to_expand.append(i)
-    return rows_to_expand, cols_to_expand
+
+    row_count = 0
+    for i in rows_to_expand:
+        input = input.splitlines()
+        input.insert(i + row_count, "." * len(input[0]))
+        input = "\n".join(input)
+        row_count += 1
+
+    col_count = 0
+    for i in cols_to_expand:
+        input = input.splitlines()
+        for j, line in enumerate(input):
+            input[j] = line[: i + col_count] + "." + line[i + col_count :]
+        input = "\n".join(input)
+        col_count += 1
+    return input
 
 
 def read_input():
@@ -41,9 +56,9 @@ def read_input():
 
 def main():
     input = read_input()
+    input = expand(input)
     for line in input.splitlines():
         print(line)
-    print(expand(input))
 
 
 if __name__ == "__main__":
