02af52954320d7075e13f9c2a98fe0c8ca8b79ab
mharv <mitch.k.harvey@gmail.com>
2023-12-14 07:55:17 +0000

    day14 attempt

--- 2023/day14/part1.py
+++ 2023/day14/part1.py
@@ -1,15 +1,66 @@
 # Description: Advent of Code: Day 14, 2023
 # Created by: Mitchell Harvey
 
-example = """"""
+example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+
+print_matrix = lambda matrix: print("\n".join([" ".join(line) for line in matrix]))
 
 
 def process_input(input):
-    return input
+    output = []
+    for line in input.splitlines():
+        output.append(list(line))
+    return output
 
 
-def apply_instructions(instructions):
-    print(instructions)
+def rotate_matrix_clockwise(matrix):
+    try:
+        return [
+            [matrix[j][i] for j in range(len(matrix) - 1, -1, -1)]
+            for i in range(len(matrix[0]))
+        ]
+    except:
+        return IndexError
+
+
+def rotate_matrix_counterclockwise(matrix):
+    try:
+        return [
+            [matrix[i][j] for i in range(len(matrix))]
+            for j in range(len(matrix[0]) - 1, -1, -1)
+        ]
+    except IndexError:
+        return IndexError
+
+
+def apply_lever(matrix):
+    swaps_made = 0
+    for row_index, row in enumerate(matrix):
+        for i, item in enumerate(row):
+            if i + 1 == len(row):
+                break
+            if item == "." and row[i + 1] == "O":
+                print(f"{item} : {row[i+1]}")
+                matrix[row_index][i], matrix[row_index][i + 1] = (
+                    matrix[row_index][i + 1],
+                    matrix[row_index][i],
+                )
+                swaps_made += 1
+                continue
+    if swaps_made > 0:
+        return apply_lever(matrix)
+    else:
+        return matrix
 
 
 def read_input():
@@ -19,8 +70,14 @@ def read_input():
 
 
 def main():
-    # apply_instructions(instructions, mapp, 0, start_position)
-    print("notuso")
+    matrix = rotate_matrix_clockwise(
+        apply_lever(rotate_matrix_counterclockwise(process_input(read_input())))
+    )
+
+    total = 0
+    for i, row in enumerate(reversed(matrix)):
+        total += row.count("O") * (i + 1)
+    print(total)
 
 
 if __name__ == "__main__":
--- 2023/day14/part2.py
+++ 2023/day14/part2.py
@@ -1,26 +1,94 @@
 # Description: Advent of Code: Day 14, 2023
 # Created by: Mitchell Harvey
 
-example = """"""
+example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+
+print_matrix = lambda matrix: print("\n".join([" ".join(line) for line in matrix]))
 
 
 def process_input(input):
-    return input
+    output = []
+    for line in input.splitlines():
+        output.append(list(line))
+    return output
 
 
-def apply_instructions(instructions):
-    print(instructions)
+def rotate_matrix_clockwise(matrix):
+    try:
+        return [
+            [matrix[j][i] for j in range(len(matrix) - 1, -1, -1)]
+            for i in range(len(matrix[0]))
+        ]
+    except:
+        return IndexError
+
+
+def rotate_matrix_counterclockwise(matrix):
+    try:
+        return [
+            [matrix[i][j] for i in range(len(matrix))]
+            for j in range(len(matrix[0]) - 1, -1, -1)
+        ]
+    except IndexError:
+        return IndexError
+
+
+def apply_lever(matrix):
+    swaps_made = 0
+    for row_index, row in enumerate(matrix):
+        for i, item in enumerate(row):
+            if i + 1 == len(row):
+                break
+            if item == "." and row[i + 1] == "O":
+                # print(f"{item} : {row[i+1]}")
+                matrix[row_index][i], matrix[row_index][i + 1] = (
+                    matrix[row_index][i + 1],
+                    matrix[row_index][i],
+                )
+                swaps_made += 1
+                continue
+    if swaps_made > 0:
+        return apply_lever(matrix)
+    else:
+        return matrix
 
 
 def read_input():
-    # return example
+    return example
     with open("./input.txt") as file:
         return file.read()
 
 
+def perform_cycle(matrix):
+    matrix = apply_lever(rotate_matrix_counterclockwise(matrix))  ## north
+    matrix = apply_lever(rotate_matrix_clockwise(matrix))  ## west
+    matrix = apply_lever(rotate_matrix_clockwise(matrix))  ## south
+    matrix = apply_lever(rotate_matrix_clockwise(matrix))  ## east
+    # return to north facing
+    matrix = rotate_matrix_clockwise(rotate_matrix_clockwise(matrix))
+    return matrix
+
+
 def main():
-    # apply_instructions(instructions, mapp, 0, start_position)
-    print("notuso")
+    matrix = process_input(read_input())
+
+    for i in range(1000000000):
+        matrix = perform_cycle(matrix)
+
+    total = 0
+    for i, row in enumerate(reversed(matrix)):
+        total += row.count("O") * (i + 1)
+    print(total)
 
 
 if __name__ == "__main__":
