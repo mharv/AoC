e7c79267513de77f6abef29c0fb57981e7ed031a
mharv <mitch.k.harvey@gmail.com>
2025-02-28 07:26:17 +1100

    day 3p art 1

---
+++ 2024/day3/src/main.rs
@@ -0,0 +1,39 @@
+use aoc_utils::get_input;
+use regex::Regex;
+
+fn get_values(expression: String) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    let mut a: String = String::new();
+    for c in expression.chars() {
+        if c == ',' || c == ')' {
+            result.push(a.parse().unwrap());
+            a = String::new();
+        }
+        if c.is_numeric() {
+            a.push(c);
+        }
+    }
+    return result;
+}
+
+fn main() {
+    let input = get_input(false);
+
+    let mut matches: Vec<String> = Vec::new();
+
+    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let found = re.find_iter(&input);
+    for item in found {
+        matches.push(item.as_str().to_string());
+    }
+
+    let mut result = 0;
+    for val in matches {
+        let nums = get_values(val);
+        result += nums.get(0).unwrap() * nums.get(1).unwrap();
+    }
+
+    println!("Part 1 is: {}", result);
+
+    // part 2
+}
