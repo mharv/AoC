595ad382f471834620d82d2e223476870f8f5b08
mharv <mitch.k.harvey@gmail.com>
2025-02-28 18:08:22 +1100

    day3 done

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -19,21 +19,52 @@ fn get_values(expression: String) -> Vec<i32> {
 fn main() {
     let input = get_input(false);
 
+    // let mut matches: Vec<String> = Vec::new();
+
+    // let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    // let found = re.find_iter(&input);
+    // for item in found {
+    //     matches.push(item.as_str().to_string());
+    // }
+
+    // let mut result = 0;
+    // for val in matches {
+    //     let nums = get_values(val);
+    //     result += nums.get(0).unwrap() * nums.get(1).unwrap();
+    // }
+
+    // println!("Part 1 is: {}", result);
+
+    // part 2
     let mut matches: Vec<String> = Vec::new();
 
-    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don\'t\(\)").unwrap();
     let found = re.find_iter(&input);
     for item in found {
+        println!("{}", item.as_str());
         matches.push(item.as_str().to_string());
     }
 
     let mut result = 0;
+    let mut toggle = true;
     for val in matches {
-        let nums = get_values(val);
-        result += nums.get(0).unwrap() * nums.get(1).unwrap();
+        match val.as_str() {
+            "do()" => {
+                toggle = true;
+                continue;
+            }
+            "don't()" => {
+                toggle = false;
+                continue;
+            }
+            _ => (),
+        }
+
+        if toggle {
+            let nums = get_values(val);
+            result += nums.get(0).unwrap() * nums.get(1).unwrap();
+        }
     }
 
-    println!("Part 1 is: {}", result);
-
-    // part 2
+    println!("Part 2 is: {}", result);
 }
